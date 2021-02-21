23b3e03daffcc452b41226bd6d97226449e70cab
Pia Balz <pia.balz@daimler.com>
2021-02-21 19:36:11 +0100

    Added API Config

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -8,6 +11,8 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
+var API_KEY = process.env.API_KEY;
+
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
@@ -15,7 +20,7 @@ app.get('/', function (req, res) {
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8080!');
 })
 
 app.get('/test', function (req, res) {

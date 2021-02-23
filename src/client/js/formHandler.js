4b3a94fcd3a87536e5a4621da0aaaa606c280ce9
Pia Balz <pia.balz@daimler.com>
2021-02-23 01:03:46 +0100

    Added script for service worker, start setting up jest

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,35 +4,30 @@ function handleSubmit(event) {
     // check what text was put into the form field
     let formText = document.getElementById('input').value;
     
-    //TODO:
-    //^(http|https)://
     let re = /^(http|https):/;
     var valid = re.test(formText);
+    
     if(!valid){
         alert("Please make sure you entered a valid URL");
+        
+    } else{
+
+        const json = { "url": formText };
+        Client.postData('http://localhost:8081/sentiment', json)
+    
+            .then(function (sentimentData) {
+                if ((sentimentData === undefined) || 
+                    (sentimentData.status.code !== '0')
+                ){
+                    console.log(sentimentData.status);
+                    document.getElementById('noresults').innerHTML = "No results found ";
+                } else {
+                    document.getElementById('noresults').innerHTML = "";
+                    console.log(sentimentData);
+                    Client.updateUI(sentimentData);
+                }
+            })
     }
-    console.log(formText);
-    console.log(valid);
-
-    const text = '{ "url" : formText.stringify() }';
-    const json = { "url": formText };
-
-    console.log("this is the json" + text);
-
-    Client.postData('http://localhost:8081/sentiment', json)
-
-        .then(function (sentimentData) {
-            if ((sentimentData === undefined) || 
-                (sentimentData.status.code !== '0')
-            ){
-                console.log(sentimentData.status);
-                document.getElementById('noresults').innerHTML = "No results found ";
-            } else {
-                document.getElementById('noresults').innerHTML = "";
-                console.log(sentimentData);
-                Client.updateUI(sentimentData);
-            }
-        })
 }
 
 export { handleSubmit }
